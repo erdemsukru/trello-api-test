58462303a9722016131d84cb773b4eed7975a416
ErdemSukru <erdemsukrudemirtas@gmail.com>
2026-02-22 09:14:03 +0300

    Token gizlendi

--- src/main/java/com/testinium/config/TrelloConfig.java
+++ src/main/java/com/testinium/config/TrelloConfig.java
@@ -1,10 +1,10 @@
 package com.testinium.config;
 
 public class TrelloConfig {
-    // Senin API Key'in
+    //  API Key
     public static final String API_KEY = "027395d9bccb110d3e0570c75c9ba183";
 
-    // Az önce aldığın token (kendi token'ını yaz)
+    // token
     public static final String TOKEN = "ATTA697458f38810e19b1e4f9b95dfc4fbb58dcf49335662a8d54779e912d84b67d7099737CC";
 
     public static final String BASE_URL = "https://api.trello.com/1";
